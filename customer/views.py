5470ca96e87f7d98304f5e47381241d6e3a73dd9
êTaeyeop Kim <ktim610@gmail.com>
2018-05-16 17:57:16 +0900

    define view action

--- customer/views.py
+++ customer/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Customer
 
 # Create your views here.
 def index(req):
-    return HttpResponse("hello world! this is customer index")
+    customers = Customer.objects.all()
+    return HttpResponse("hello world! this is customer index</br>"+', '.join([c.name for c in customers]))
+
+def upload(req):
+    return HttpResponse("upload!")
+
+def list(req):
+    return HttpResponse("list!")
