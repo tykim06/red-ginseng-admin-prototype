4eeda9ae07399a262f73d4626d1dd8bee2f13744
êTaeyeop Kim <ktim610@gmail.com>
2018-05-16 17:04:00 +0900

    define repesent string of customer model

--- customer/models.py
+++ customer/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
